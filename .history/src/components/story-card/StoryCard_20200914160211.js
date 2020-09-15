607c1e60ba108225c79de2ce82580af20bb24550
marcinszymaszek <marcinszymaszek@wp.pl>
2020-09-15 10:31:55 +0200

    Created a story card

---
+++ .history/src/components/story-card/StoryCard_20200914160211.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const StoryCard = () => {
+    return (
+        <section>
+            <div className="container-outter">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12" id="story-card-module">
+                            <div className="img-container">
+                                <img className="container-img" src={require("../../styles/story_card.jpg")} alt="story card image" />
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-6 col-sm-12" id="header-module">
+                            <div className="container-text">
+                                <h1 className="container-title">
+                                    Every evening can become a great movie night!
+                                </h1>
+                                <h2 className="container-title">
+                                    Discover movies, series, and shows that you can watch together.
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default StoryCard;
---
+++ .history/src/components/story-card/StoryCard_20200914165812.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const StoryCard = () => {
+    return (
+        <section className="story-card">
+            <div className="container-outter">
+                <div className="container-fluid">
+                    <div className="row">
+
+                        <div className="col-lg-6 col-md-12 col-sm-12" id="story-card-module">
+                            <div className="container-text">
+                                <h2 className="story-card-title">
+                                    Every evening can become a great movie night!
+                                </h2>
+                                <h3 className="story-card-subtitle">
+                                    Discover movies, series, and shows that you can watch together.<br />
+                                        Save your favorite titles so you always have something to watch and watch on all your devices
+                                </h3>
+                            </div>
+                        </div>
+
+                        <div className="col-lg-6 col-md-12 col-sm-12" id="story-card-module">
+                            <div className="story-card-img-container">
+                                <img className="story-card-img" src={require("../../styles/story_card.png")} alt="story card" />
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default StoryCard;
